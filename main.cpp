66378b1e4f668c86a71bab2eb4793b55268ffe69
adriankalinowski95 <adrian.kalinowski95@gmail.com>
2024-03-02 11:49:19 +0100

    init.

---
+++ main.cpp
@@ -0,0 +1,17 @@
+// 1. Stworzyc projekt w cmake.
+// 2?. Dodac do niego conana
+// 3. Exceptiony w watkach
+// 4. RAII w tym wylaczeine watkow
+// 5. Przekazywanie obiektow przez referencje (std::ref)
+// 6. condition_veriables
+// 7. std::future itp.
+// 8. jak projektowac(taki dzial, chyba nawet 8)
+
+#include <iostream>
+#include <ranges>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello world" << std::endl;
+
+    return 1;
+}
